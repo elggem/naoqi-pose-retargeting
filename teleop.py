41caa1a1a0943a52f5bd250dfb0b9c07cdd85568
Ralf Mayet <trilly@gmail.com>
2021-10-15 14:19:20 +0200

    fix elbow jaw

--- teleop.py
+++ teleop.py
@@ -12,8 +12,9 @@ import argparse
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
+import matplotlib.pyplot as plt
 
-from time import sleep
+from time import time, sleep
 
 from utils import CvFpsCalc
 from utils import KeypointsToAngles
@@ -23,6 +24,8 @@ from utils import calc_bounding_rect, draw_landmarks, plot_world_landmarks, draw
 
 keypointsToAngles = KeypointsToAngles()
 
+angle_trace = []
+
 def get_args():
     parser = argparse.ArgumentParser()
 
@@ -31,6 +34,7 @@ def get_args():
     parser.add_argument("--height", help='cap height', type=int, default=540)
 
     parser.add_argument("--video", type=str, default="")
+    parser.add_argument("--fps", type=int, default=10)
 
     parser.add_argument("--model_complexity",
                         help='model_complexity(0,1(default),2)',
@@ -46,6 +50,7 @@ def get_args():
                         default=0.5)
     parser.add_argument('--use_brect', action='store_true')
     parser.add_argument('--plot_world_landmark', action='store_true')
+    parser.add_argument('--plot_angle_trace', action='store_true')
     
     parser.add_argument('--enable_teleop', action='store_true')
 
@@ -55,6 +60,8 @@ def get_args():
 
 
 def main():
+    global angle_trace
+
     args = get_args()
 
     cap_device = args.device
@@ -67,9 +74,11 @@ def main():
 
     use_brect = args.use_brect
     plot_world_landmark = args.plot_world_landmark
+    plot_angle_trace = args.plot_angle_trace
     enable_teleop = args.enable_teleop
 
     video = args.video
+    fps = args.fps
 
     if len(video) == 0:
         cap = cv.VideoCapture(cap_device)
@@ -95,14 +104,12 @@ def main():
     cvFpsCalc = CvFpsCalc(buffer_len=10)
 
     if plot_world_landmark:
-        import matplotlib.pyplot as plt
-        
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
         fig.subplots_adjust(left=0.0, right=1, bottom=0, top=1)
 
     while True:
-        sleep(0.06)
+        start = time()
 
         display_fps = cvFpsCalc.get()
 
@@ -124,15 +131,15 @@ def main():
             if results.pose_world_landmarks is not None:
                 plot_world_landmarks(plt, ax, results.pose_world_landmarks)
 
-        if enable_teleop:
-            if results.pose_world_landmarks is not None:
-                cv.putText(debug_image, "TRACKING", (cap_width-100, 30),
-                   cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2, cv.LINE_AA)
-                if not do_teleop(results.pose_world_landmarks):
-                    # limit reached
-                    cv.putText(debug_image, "LIMIT", (cap_width-100, 90),
-                        cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2, cv.LINE_AA)
+        if results.pose_world_landmarks is not None:
+            cv.putText(debug_image, "TRACKING", (cap_width-100, 30),
+                cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2, cv.LINE_AA)
+            if not do_teleop(results.pose_world_landmarks):
+                # limit reached
+                cv.putText(debug_image, "LIMIT", (cap_width-100, 90),
+                    cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2, cv.LINE_AA)
 
+            if enable_teleop:
                 socket_stream_landmarks(ss, results.pose_world_landmarks)
 
         cv.putText(debug_image, "FPS:" + str(display_fps), (10, 30),
@@ -144,6 +151,16 @@ def main():
 
         cv.imshow('Pepper Teleop', debug_image)
 
+        if (len(video)>0):
+            sleep(max(1./fps - (time() - start), 0))
+
+    if plot_angle_trace:
+        angle_labels = ["LShoulderPitch","LShoulderRoll", "LElbowYaw", "LElbowRoll", "RShoulderPitch","RShoulderRoll", "RElbowYaw", "RElbowRoll", "HipPitch"]
+        angle_trace = np.array(angle_trace)
+        lines = plt.plot(angle_trace[:,:])
+        plt.legend(iter(lines), angle_labels[:])
+        plt.show()
+
     cap.release()
     cv.destroyAllWindows()
 
@@ -153,15 +170,14 @@ def socket_stream_landmarks(ss, landmarks):
         p.append([landmark.x, landmark.y, landmark.z])
     # p = np.array(p)
 
-    pNeck =   (np.array(p[11]) + np.array(p[12])).tolist()
-    pMidHip = (np.array(p[23]) + np.array(p[24])).tolist()
+    pNeck =   (0.5 * (np.array(p[11]) + np.array(p[12]))).tolist()
+    pMidHip = (0.5 * (np.array(p[23]) + np.array(p[24]))).tolist()
     
     wp_dict = {}
 
     wp_dict['0'] = p[0]    # Nose
     wp_dict['1'] = pNeck   # Neck
     
-
     wp_dict['2'] = p[11]   # LShoulder
     wp_dict['3'] = p[13]   # LElbow
     wp_dict['4'] = p[15]   # LWrist
@@ -178,7 +194,9 @@ def socket_stream_landmarks(ss, landmarks):
 def checkLim(val, limits):
     return val < limits[0] or val > limits[1]
 
+
 def do_teleop(landmarks):
+    global angle_trace
     p = []
     for index, landmark in enumerate(landmarks.landmark):
         p.append([landmark.x, landmark.y, landmark.z])
@@ -194,18 +212,21 @@ def do_teleop(landmarks):
     limitsRElbowRoll     = [ 0.0087, 1.5620]
     limitsHipPitch       = [-1.0385, 1.0385]
 
+    pNeck =   (0.5 * (np.array(p[11]) + np.array(p[12]))).tolist()
+    pMidHip = (0.5 * (np.array(p[23]) + np.array(p[24]))).tolist()
+
     LShoulderPitch, LShoulderRoll = keypointsToAngles.obtain_LShoulderPitchRoll_angles(p[11]+p[12], p[11], p[13], p[23]+p[24])
     RShoulderPitch, RShoulderRoll = keypointsToAngles.obtain_RShoulderPitchRoll_angles(p[11]+p[12], p[12], p[14], p[23]+p[24])
     
-    LElbowYaw, LElbowRoll = keypointsToAngles.obtain_LElbowYawRoll_angle(p[11]+p[12], p[11], p[13], p[15]) # yaw untested
-    RElbowYaw, RElbowRoll = keypointsToAngles.obtain_RElbowYawRoll_angle(p[11]+p[12], p[12], p[14], p[16]) # yaw untested
+    LElbowYaw, LElbowRoll = keypointsToAngles.obtain_LElbowYawRoll_angle(pNeck, p[11], p[13], p[15])
+    RElbowYaw, RElbowRoll = keypointsToAngles.obtain_RElbowYawRoll_angle(pNeck, p[12], p[14], p[16])
 
-    HipPitch = keypointsToAngles.obtain_HipPitch_angles(p[11]+p[12], p[23]+p[24])
+    HipPitch = keypointsToAngles.obtain_HipPitch_angles(pNeck, pMidHip)
 
-    print([LShoulderPitch,LShoulderRoll, LElbowYaw, LElbowRoll, \
-                 RShoulderPitch,RShoulderRoll, RElbowYaw, RElbowRoll, \
-                 HipPitch])
+    angles = [LShoulderPitch,LShoulderRoll, LElbowYaw, LElbowRoll, RShoulderPitch,RShoulderRoll, RElbowYaw, RElbowRoll, HipPitch]
 
+    print(angles)
+    angle_trace.append(angles)
 
     if (checkLim(LShoulderPitch, limitsLShoulderPitch) or 
         checkLim(RShoulderPitch, limitsRShoulderPitch) or
